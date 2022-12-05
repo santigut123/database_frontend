00067265a36eaf989a97f8b9c5c5a3f049b98281
mshpo <nickshpo@gmail.com>
2022-12-04 23:15:05 -0500

    Finish enlistedSurveys component.

--- components/enlistedSurveys.tsx
+++ components/enlistedSurveys.tsx
@@ -1,22 +1,49 @@
 
 import {Flex,Text,Button,Stack} from "@mantine/core"
-import { ReactElement } from "react"
-function enlistedSurveyElement(surveyObject: any){
+import { getAdditionalUserInfo } from "firebase/auth"
+import { ReactElement, useEffect, useState } from "react"
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useRouter } from 'next/router'
+
+function enlistedSurveyElement(surveyObject: any) {
+    const router = useRouter()
     return (
-        <Flex id={surveyObject.ID}>
-            <Text>{surveyObject.surveyName}</Text>
-            <Button>Take Survey</Button>
+        <Flex id={surveyObject.survey_id}>
+            <Text>{surveyObject.title}</Text>
+            <Button onClick={() => router.push(`/${surveyObject.survey_id}`)}>Take Survey</Button>
         </Flex>
     )
-
 }
 
-export default function enlistedSurveys():ReactElement{
-    const surveys=[{surveyName:"Survey 1",ID:"test123"},{surveyName:"Survey 2",ID:"test122"},{surveyName:"Survey 3",ID:"test121"}]
+export default function enlistedSurveys():ReactElement {
+    const [surveys, setAssignedSurveys] = useState<any[]>([])
+    const supabase = useSupabaseClient()
+
+    async function getUserId() {
+        const { data: { user } } = await supabase.auth.getUser()
+        return user?.id
+    }
+
+    async function getAssignedSurveys() {
+        const input_user_id = await getUserId()
+
+        let { data, error } = await supabase
+        .rpc('getassignedsurveys', {
+            input_user_id
+        })
+
+        if (error) console.error(error)
+
+        setAssignedSurveys(data);
+    }
+
+    useEffect(() => {
+        getAssignedSurveys();
+    }, [])
+
     return (
         <Stack>
          {surveys.map((survey)=>enlistedSurveyElement(survey))}
         </Stack>
-
     )
 }
