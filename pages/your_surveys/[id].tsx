261b89ff3f0cf638255126ff91e816c22ed7295f
Santiago Gutierrez <santigut@posteo.net>
2022-12-04 19:40:26 -0500

    added new pages etc

---
+++ pages/your_surveys/[id].tsx
@@ -0,0 +1,49 @@
+import {useRouter} from 'next/router'
+
+import {Dispatch, ReactElement, SetStateAction, useState} from 'react'
+import {Stack,TextInput,Textarea,Title,Button,Flex,Text,NativeSelect} from '@mantine/core'
+import { isLabeledStatement } from 'typescript'
+
+function makeSurvey(){
+
+}
+interface question{
+    question:string,
+    type:string,
+    result:string
+
+}
+function renderQuestion(question:any,index:any,setQuestions:any,questions:any):ReactElement{
+    if (question.type==="two"){
+        return(
+        <Flex key={index}>
+                <Textarea onChange={(event)=>question.result=event.currentTarget.value} label={question.question}></Textarea>
+            </Flex>
+        )
+    }
+    else{
+        return(<Flex key={index}>
+            <NativeSelect  data={['1','2','3','4','5'] } onChange={(event)=>question.result=event.currentTarget.value} label={question.question} description={"on a scale from 1-5"}></NativeSelect>
+        </Flex>)
+
+    }
+   
+}
+function submitSurvey(router:any){
+    router.push("/dashboard")
+
+}
+export default function yourSurvey() {
+  const router = useRouter()
+  const { id } = router.query
+  const results = Array<question>({question:"How was your day?",type:"one",result:""},{question:"What did you do today?",type:"two",result:""})
+  const [questions,setQuestions]=useState(results)
+
+  console.log(questions)
+  return(
+    <Stack sx={{margin:"20px"}}>
+    {questions.map((question:any,index:any)=>renderQuestion(question,index,setQuestions,questions))}
+    <Button onClick={()=>submitSurvey(router)}>Submit</Button>
+    </Stack>
+  )
+}
